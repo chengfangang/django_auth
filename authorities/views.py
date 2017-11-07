29ca8a1e1f40ea08b3be9c58efe9ad35faed778e
chengfangang <chengfangang@etongdai.com>
2017-11-08 04:43:15 +0800

    add basic content

--- authorities/views.py
+++ authorities/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+#from django.http import HttpResponse
 
 # Create your views here.
+
+def appsettings(request):
+    return render(request, 'authorities/appsettings.html')
